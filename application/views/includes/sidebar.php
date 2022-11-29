c50bff7a8a050dff475bb970f30bacd7943eaafe
Jesus Cruz <jesus.cruz@impact.cl>
2022-11-29 05:10:10 -0300

    Little details updated in the sidebar view.

--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -1,61 +1,62 @@
 <!-- **********************************************************************************************************************************************************
       MAIN SIDEBAR MENU
       *********************************************************************************************************************************************************** -->
-      <!--sidebar start-->
-      <aside>
-          <div id="sidebar"  class="nav-collapse ">
-              <!-- sidebar menu start-->
-              <ul class="sidebar-menu" id="nav-accordion">
-              
-                <p class="centered"><a href="profile.html"><img src="<?= base_url() ?>assets/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
-              	<h5 class="centered">
-                    <?php
-                        if (isset($_SESSION['Nombre']) && isset($_SESSION['Apellidos'])) {
-                            echo strtoupper($_SESSION['Nombre']." ".$_SESSION['Apellidos']);
-                        }
-                    ?>
-                </h5>
-              	  	
-                <li class="mt">
-                    <a class="active" href="<?= base_url() ?>Dashboard">
-                        <i class="fa fa-dashboard"></i>
-                        <span>Dashboard</span>
-                    </a>
-                </li>
+<!--sidebar start-->
+<aside>
+    <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu" id="nav-accordion">
 
+            <p class="centered"><a href="profile.html"><img src="<?= base_url() ?>assets/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
+            <h5 class="centered">
                 <?php
-                    if ($_SESSION['tipo'] == 'Profesor') {
+                if (isset($_SESSION['Nombre']) && isset($_SESSION['Apellidos'])) {
+                    echo strtoupper($_SESSION['Nombre'] . " " . $_SESSION['Apellidos']);
+                }
                 ?>
-                        <li class="sub-menu">
-                            <a href="<?= base_url() ?>Dashboard/gestionAlumnos" >
-                                <i class="fa fa-list"></i>
-                                <span>Gestión de Alumnos</span>
-                            </a>
-                        </li>
-                        <li class="sub-menu">
-                            <a href="<?= base_url() ?>Dashboard/crearTareas">
-                                <i class="fa fa-plus"></i>
-                                <span>Crear Tareas</span>
-                            </a>
-                        </li>
-                <?php
-                    }
-                ?>
+            </h5>
 
+            <li class="mt">
+                <a href="<?= base_url() ?>Dashboard">
+                    <i class="fa fa-dashboard"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <?php
+            if ($_SESSION['tipo'] == 'Profesor') {
+            ?>
                 <li class="sub-menu">
-                    <a href="<?= base_url() ?>Dashboard/misTareas" >
-                        <i class="fa fa-book"></i>
-                        <span>Mis Tareas</span>
+                    <a href="<?= base_url() ?>Dashboard/gestionAlumnos">
+                        <i class="fa fa-list"></i>
+                        <span>Gestión de Alumnos</span>
                     </a>
                 </li>
                 <li class="sub-menu">
-                    <a href="javascript:;" >
-                        <i class="fa fa-comment"></i>
-                        <span>Mensajes</span>
+                    <a href="<?= base_url() ?>Dashboard/crearTareas">
+                        <i class="fa fa-plus"></i>
+                        <span>Crear Tareas</span>
                     </a>
                 </li>
-              </ul>
-              <!-- sidebar menu end-->
-          </div>
-      </aside>
-      <!--sidebar end-->
+            <?php
+            }
+            ?>
+
+            <li class="sub-menu">
+                <a href="<?= base_url() ?>Dashboard/misTareas">
+                    <i class="fa fa-book"></i>
+                    <span>Mis Tareas</span>
+                </a>
+            </li>
+            
+            <li class="sub-menu">
+                <a href="<?= base_url() ?>Dashboard/mensajes">
+                    <i class="fa fa-comment"></i>
+                    <span>Mensajes</span>
+                </a>
+            </li>
+        </ul>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
+<!--sidebar end-->
