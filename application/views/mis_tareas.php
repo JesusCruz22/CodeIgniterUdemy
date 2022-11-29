e774a4e9bc79a67a41e48b076f2780021e551262
Jesus Cruz <jesus.cruz@impact.cl>
2022-11-29 13:39:34 -0300

    Fix tasks view to control a user without tasks.

--- application/views/mis_tareas.php
+++ application/views/mis_tareas.php
@@ -3,34 +3,41 @@
 
         <div class="row"></div>
         <?php
-        foreach ($tareas as $tarea) {
+        if ($tareas) {
+            foreach ($tareas as $tarea) {
         ?>
-            <div class="col-lg-4 col-md-4 col-sm-4 mb">
-                <div class="white-panel pn">
-                    <div class="white-header">
-                        <h4><?= $tarea->Titulo ?></h4>
-                    </div>
-                    <div class="row" style="height: 50%;">
-                        <?= $tarea->Descripcion ?>
-                    </div>
-                    <div class="row align-items-end" style="margin-bottom: .1em;">
-                        <small>Fecha Limite:</small><?= date('d-m-Y', strtotime($tarea->FechaFinal)) ?>
-                    </div>
-                    <div class="row align-items-end">
-                        <?php if ($tarea->Archivo != "") { ?>
-                            <a class="btn btn-default" href="<?= base_url() . 'uploads/' . $tarea->Archivo ?>" download="">
-                                <i class="fa fa-download"></i>
-                                Descargar Archivo Adjunto
-                            </a>
-                        <?php } else { ?>
-                            <div>"Sin Archivo Adjunto"</div>
-                        <?php } ?>
+                <div class="col-lg-4 col-md-4 col-sm-4 mb">
+                    <div class="white-panel pn">
+                        <div class="white-header">
+                            <h4><?= $tarea->Titulo ?></h4>
+                        </div>
+                        <div class="row" style="height: 50%;">
+                            <?= $tarea->Descripcion ?>
+                        </div>
+                        <div class="row align-items-end" style="margin-bottom: .1em;">
+                            <small>Fecha Limite:</small><?= date('d-m-Y', strtotime($tarea->FechaFinal)) ?>
+                        </div>
+                        <div class="row align-items-end">
+                            <?php if ($tarea->Archivo != "") { ?>
+                                <a class="btn btn-default" href="<?= base_url() . 'uploads/' . $tarea->Archivo ?>" download="">
+                                    <i class="fa fa-download"></i>
+                                    Descargar Archivo Adjunto
+                                </a>
+                            <?php } else { ?>
+                                <div>"Sin Archivo Adjunto"</div>
+                            <?php } ?>
+                        </div>
                     </div>
                 </div>
+            <?php
+            }
+        } else {
+            ?>
+            <div>
+                <h3>No hay tareas asignadas.</h3>
             </div>
         <?php
         }
         ?>
-
     </section>
 </section>
